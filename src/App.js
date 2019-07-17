713b9a568f4bf9e1922bd97fda895c7c0a02d173
Jacob <jbmenashi@gmail.com>
2019-07-17 14:56:08 -0400

    add potential routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       <h1>Moon Points!</h1>
+         <Router>
+            <>
+            <h1>Moon Points!</h1>
+            <Route exact path="/" render={() => (this.props.currentUserId !== undefined ? (<Redirect to="/home"/>) : (<Login/>))}/>
+            <Route path="/home" component={HomeContainer}/>
+            <Route path="/standings" component={StandingsContainer}/>
+            <Route path="/stats" component={StatsContainer}/>
+            </>
+         </Router>
     </div>
   );
 }
