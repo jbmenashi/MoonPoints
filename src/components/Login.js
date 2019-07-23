6f6c5e690d3cf6b122b2119fabce4607ca3f6638
Jacob <jbmenashi@gmail.com>
2019-07-23 10:41:26 -0400

    functions for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { stat } from 'fs';
-
+import { connect } from 'react-redux';
+import { Input, Button } from 'semantic-ui-react'
+ 
 const mapStateToProps = state => {
    return {
       currentUsername: state.currentUsername,
       currentUserId: state.currentUserId,
-      usernameInput: stat.usernameInput
+      usernameInput: state.usernameInput
    }
 }
 
 const mapDispatchToProps = dispatch => {
    return {
       setUserId: (id, username) => dispatch({type: "SET_USER", id: id, username: username}),
-      inputUsername: (input) => dispatch({type: "INPUT_USERNAME", payload: input})
+      inputUsername: (input) => dispatch({type: "INPUT_USERNAME", payload: input.target.value})
    }
 }
 
 class Login extends Component {
 
+   submitUsername = (input) => {
+      console.log(input)
+   }
 
    render() { 
       return ( 
          <div>
             <h3>Login</h3>
-            <input type="text" onChange={this.props.inputUsername} value={this.props.usernameInput} placeholder="Username"/><br/>
+            <Input onChange={this.props.inputUsername} value={this.props.usernameInput} placeholder="Username"/>
+            <Button onClick={() => this.submitUsername(this.props.usernameInput)}>Submit</Button>
          </div>
        );
    }
